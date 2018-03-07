2f8df9ac350a11551c4a527104253507a33d4d61
Yuriy Tumakha <tumakha@gmail.com>
2018-03-07 14:10:26 +0000

    Spring Boot project initialisation

---
+++ src/main/java/com/tantalumcorporation/evChargingAPI/EvChargingApiApplication.java
@@ -0,0 +1,12 @@
+package com.tantalumcorporation.evChargingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvChargingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvChargingApiApplication.class, args);
+	}
+}
